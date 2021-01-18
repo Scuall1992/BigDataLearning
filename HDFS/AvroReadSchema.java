e8dd3e247862ce3fdc9321b65511ad8de86252f1
Scuall1992 <scuall1992@gmail.com>
2021-01-18 16:45:57 +0300

    Parse avro schema from hdfs

--- HDFS/AvroReadSchema.java
+++ HDFS/AvroReadSchema.java
@@ -1,24 +1,44 @@
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.file.SeekableInput;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) throws IOException {
+        Configuration conf = new Configuration();
 
+        FileSystem fs = FileSystem.get(conf);
 
-        String path = "/Users/Andrei_Pak/Work/hadoop/expedia/part-00022-ef2b800c-0702-462d-b37f-5f2fb3a093d0-c000.avro";
+        String path = "hdfs:///expedia/part-00022-ef2b800c-0702-462d-b37f-5f2fb3a093d0-c000.avro";
 
-        DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
-        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(new File(path), datumReader);
-        Schema schema = dataFileReader.getSchema();
-        System.out.println(schema);
+        Path file = new Path(path);
+
+        if(!fs.exists(file)){
+            System.out.println("Input file not found!");
+            throw new IOException("No file");
+        }
+
+        FSDataInputStream in = fs.open(file);
+
+        DataFileStream<GenericRecord> data = new DataFileStream<>(in, new GenericDatumReader<GenericRecord>());
+
+        Schema ret = data.getSchema();
+
+        System.out.println(ret);
+
+        data.close();
+
 
 
     }
 }
-
